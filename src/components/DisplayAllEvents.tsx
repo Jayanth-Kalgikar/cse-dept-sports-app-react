f3d61ec948c94a0a867aeff1ebfe412c96a2cf9d
Jayanth Kalgikar <jayanthkalgikar2005@gmail.com>
2025-02-28 12:43:38 +0530

    first commit

---
+++ src/components/DisplayAllEvents.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+export default function DisplayAllEvents() {
+  return (
+    <div>
+      
+      <NavBar />
+      DisplayAllEvents</div>
+  )
+}
