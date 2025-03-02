0ced989babbf272839db1842d234b34c1d0b25bb
Jayanth Kalgikar <74163955+Jayanth-Kalgikar@users.noreply.github.com>
2025-03-02 17:59:28 +0530

    Updated DisplayAll

---
+++ src/components/DisplayAllEvents.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+export default function DisplayAllEvents() {
+  return (
+    <div>
+      
+      <NavBar />
+      <img src="https://static.footballcounter.com/wp-content/uploads/2024/08/26022220/SUBROTO-U-15-QF-2-e1724599361848-872x520.jpeg" alt="A State Level *FOOTBALL* tournament was held during the college fest that is held every year. Sportsman - ship is acknowledged and highly supported in this college. " />
+      <p><strong>A State Level FOOTBALL tournament was held during the college fest that is held every year. Sportsmanship is acknowledged and highly supported in this college.</strong></p>
+      
+      </div>
+  )
+}
